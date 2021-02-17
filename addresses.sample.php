52295411c2c0e2300beadb5ac94e35e6e00067e2
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-02-17 12:24:43 +0100

    All the files

---
+++ addresses.sample.php
@@ -0,0 +1,13 @@
+<?php
+$addresses_phones = [ // These are the phones which are more important since 
+    //the pc could be on but the person not at home
+
+    "Someones mac address" => "His name",
+    "4I:34:08:1D:7D:99" => "Tim"
+];
+
+$addresses = [ // These are all the devices
+    "2I:OO:92:D13:8A:OO" => "Brother Phone",
+    "I6:I6:12:6E:15I:52" => "Litle brother phone",
+    "010:37:F12:92:E8:89" => "Little brother laptop"
+];
---
+++ index.php
@@ -0,0 +1,53 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+<style>
+    table {
+        margin: 1%;
+    }
+</style>
+<table class="table">
+    <thead>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="col">At home</th>
+        </tr>
+
+
+        <?php
+
+        $output = shell_exec("./myscript.sh");
+        include_once('addresses.php'); // Import the addresses
+
+
+        foreach ($addresses_phones as $key => $address) {
+            if (strpos($output, $key) !== false) { // Don't know why you have to do it like this
+                print "<tr><td>$address</td></tr>";
+            }
+        }
+
+        ?>
+    </tbody>
+</table>
+<table class="table">
+    <thead>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="col">All devices</th>
+        <tr>
+
+
+            <?php
+
+            foreach ($addresses as $key => $address) {
+                if (strpos($output, $key) !== false) { // Don't know why you have to do it like this
+                    print "<tr><td>$address</td></tr>";
+                }
+            }
+
+            ?>
+    </tbody>
+</table>
